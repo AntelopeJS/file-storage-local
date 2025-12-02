a0b0c31e771181548c08619f7563e8b4cebb3736
Antony Rizzitelli <upd4ting@gmail.com>
2025-12-02 15:12:16 +0100

    chore: remove dist

--- dist/routes/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { HTTPResult, RequestContext } from '@ajs/api/beta';
-declare const FileStorageController_base: import("@ajs/api/beta").ControllerClass<object>;
-export declare class FileStorageController extends FileStorageController_base {
-    handleUpload(token: string, contentType: string | undefined, contentLength: string | undefined, body: Buffer, context: RequestContext): Promise<HTTPResult>;
-    handleDownload(token: string | undefined, context: RequestContext): Promise<HTTPResult>;
-}
-export {};
